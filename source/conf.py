fa01f03c118fd930f6eb3757c7403afbd214777c
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2025-08-31 16:26:22 +0200

    fix: Update copyright year format in Sphinx configuration

--- source/conf.py
+++ source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "vuks.dev"
-copyright = "2025, Vuks"
+copyright = "%Y, Vuks"
 author = "Vuks"
 
 # -- General configuration ---------------------------------------------------
